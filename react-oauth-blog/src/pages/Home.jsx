e521c715d3de4315c6d1fe06357469c9ab62af50
Damon-Jo <lunaticjky@gmail.com>
2024-01-28 23:28:14 -0800

    added homepage components and styling with card component

--- react-oauth-blog/src/pages/Home.jsx
+++ react-oauth-blog/src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {posts} from '../data'
+import Card from '../components/Card'
 
-function Home() {
+const Home = ()=> {
   return (
-    <div>Home</div>
+    <div className='home'>
+        {posts.map(post=>(
+         <Card key={post.id} post={post}/> 
+        ))}
+    </div>
   )
 }
 
