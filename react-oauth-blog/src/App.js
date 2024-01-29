6263bd1e6be8820f9bbea85bf98932d6da834180
Damon-Jo <lunaticjky@gmail.com>
2024-01-29 00:01:57 -0800

    created login page and styling

--- react-oauth-blog/src/App.js
+++ react-oauth-blog/src/App.js
@@ -1,11 +1,18 @@
 import Navbar from "./components/Navbar";
 import "./app.css"
 import Home from "./pages/Home";
+import Post from './pages/Post';
+import Login from "./pages/Login";
+
+
 function App() {
   return (
     <div>
       <Navbar/>
-      <Home/>
+      {/* <Home/> */}
+      {/* <Post/> */}
+
+      <Login/>
       </div>
   );
 }
